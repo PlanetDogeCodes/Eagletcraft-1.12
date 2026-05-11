a8fc0dbd3384223cd4a3c9cf3da95de32b854c35
Planet Doge <142177581+PlanetDogeCodes@users.noreply.github.com>
2026-05-11 10:25:59 -0600

    Update loader.js

--- source file/loader.js
+++ source file/loader.js
@@ -29,7 +29,7 @@
             value.servers = [
   { addr: "wss://arch.mc", name: "ArchMC" },
   { addr: "wss://1b2t.eagler.host", name: "1 Builder 2 Tools" },
-  { addr: "https://github.com/PlanetDogeCodes", name: "This Eagletcraft Loader created by Planet_Doge" }
+  { addr: "https://github.com/PlanetDogeCodes", name: "This Eaglercraft Loader created by Planet_Doge" }
 ];
             this._eaglercraftXOpts = value;
           },
